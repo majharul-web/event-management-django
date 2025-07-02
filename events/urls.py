518144a109c862d3487e239dc4ee53ea8289dfbe
majharul <majharuljony163@gmail.com>
2025-07-02 16:23:34 +0600

    basic structur design done

--- events/urls.py
+++ events/urls.py
@@ -1,8 +1,34 @@
 from django.urls import path
-from events.views import home, about
+from . import views
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('home/', home, name='home'),
-    path('about/', about, name='about'),
+    path('', views.home, name='home'),
+    path('/home', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    
+    path('events/<int:pk>/', views.event_detail, name='event_detail'),
+
+
+    # Dashboard
+    path('dashboard/', views.dashboard, name='dashboard'),
+
+    # Event URLs
+    path('events/', views.event_list, name='event_list'),
+    path('add-event/', views.event_create, name='event_create'),
+    path('events/<int:pk>/edit/', views.event_update, name='event_update'),
+    path('events/<int:pk>/delete/', views.event_delete, name='event_delete'),
+
+    # Participant URLs
+    path('participants/', views.participant_list, name='participant_list'),
+    path('participants/add/', views.participant_create, name='participant_create'),
+    path('participants/<int:pk>/edit/', views.participant_update, name='participant_update'),
+    path('participants/<int:pk>/delete/', views.participant_delete, name='participant_delete'),
+   
+
+
+    # Category URLs
+    path('categories/', views.category_list, name='category_list'),
+    path('categories/add/', views.category_create, name='category_create'),
+    path('categories/<int:pk>/edit/', views.category_update, name='category_update'),
+     path('categories/<int:pk>/delete/', views.category_delete, name='category_delete'),
 ]
